94d4a53582d01927c053c6769f9be43f091241ac
Gaurav <2022wb86313@wilp.bits-pilani.ac.in>
2026-09-21 12:57:48 +0000

    git add git commit -m "Intial Maven Project for DevOps Lab Sheet 2" git branch -M main git remote add origin https://github.com/Gauarvj1109/2022WB86313 git push -u origin main

---
+++ SampleApp.java
@@ -0,0 +1,7 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("DevOps Continuous Integration Lab");
+        System.out.println("BITS ID: 2022WB86313");
+    }
+}
